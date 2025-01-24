75b6a2208edae606f1b9c81d8dc10adb39d25213
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-01-24 18:40:41 -0500

    feat: First commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Andersson on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
