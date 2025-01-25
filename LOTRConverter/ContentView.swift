67d7dbdad5e3ce527baf9fcaf6bde46a85d81c27
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-01-24 20:58:41 -0500

    feat: Home Screen Layout

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeInfo = false
+    @State var topAmount = ""
+    @State var bottomAmount = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                VStack {
+                    VStack {
+                        HStack {
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 60)
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        TextField("Amount", text: $topAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                    }
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                        .padding()
+                    VStack {
+                        TextField("Amount", text: $bottomAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                        HStack {
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 60)
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
+            }
         }
-        .padding()
     }
 }
 
